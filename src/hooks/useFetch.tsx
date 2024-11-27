02401aac7462aaf9502c054bbaf974fa571befbc
SalimCanTetiker <cantetiker@gmail.com>
2024-11-27 13:43:58 +0300

    useFetch hook created

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,26 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const useFetch = (url) => {
+
+    const [data, setData] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const fetchData = async () => {
+        try {
+            const {data: responseData} = await axios.get(url)
+            setData(responseData)
+            setLoading(false)
+        } catch (error) {
+            setError(error.message)
+            setLoading(false)
+        }
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+    return { data, error, loading }
+}
+
+export default useFetch
--- src/screens/SettingsPage/SettingsPage.tsx
+++ src/screens/SettingsPage/SettingsPage.tsx
@@ -1,9 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
+import useFetch from '../../hooks/useFetch'
 
 const SettingsPage = () => {
+
+  const {data} = useFetch("https://run.mocky.io/v3/104eca1a-b24d-497e-92bd-a6fa241e0524")
+
+  const renderData = ({item}) => {
+    return (
+      <View>
+        <Text>{item.name}</Text>
+      </View>
+    )
+  }
   return (
     <View style={{backgroundColor: "#489bff", flex: 1}}>
       <Text>SettingsPage</Text>
+      <FlatList data={data} renderItem={renderData} />
     </View>
   )
 }
